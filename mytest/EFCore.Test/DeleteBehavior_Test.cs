60571fcb8f5a4da14d585cc7d0e84098efbb7ebd
tangdf <87925827@qq.com>
2018-05-25 17:20:28 +0800

    no message

--- mytest/EFCore.Test/DeleteBehavior_Test.cs
+++ mytest/EFCore.Test/DeleteBehavior_Test.cs
@@ -3,19 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace EFCore.Test
 {
     public class DeleteBehavior_Test
     {
+        private ITestOutputHelper _testOutputHelper;
 
-        public DeleteBehavior_Test()
+
+        public DeleteBehavior_Test(ITestOutputHelper testOutputHelper)
         {
-           
+            _testOutputHelper = testOutputHelper;
         }
 
 
+
+        //https://docs.microsoft.com/zh-cn/previous-versions/sql/sql-server-2005/ms186973(v%3dsql.90)
+        //https://docs.microsoft.com/zh-cn/ef/core/saving/cascade-delete
+        //https://github.com/aspnet/EntityFramework.Docs/blob/live/entity-framework/core/saving/cascade-delete.md
+        //https://github.com/aspnet/EntityFramework.Docs.zh-cn/blob/live/entity-framework/core/saving/cascade-delete.md
+
+
+
+        [InlineData(DeleteBehavior.Cascade)]
+        [InlineData(DeleteBehavior.SetNull)]
+        [InlineData(DeleteBehavior.ClientSetNull)]
+        [InlineData(DeleteBehavior.Restrict)]
+        [Theory]
+        public void Create_Database(DeleteBehavior behavior)
+        {
+            using (var northwindContext = new NorthwindContext(behavior))
+            {
+                northwindContext.Database.EnsureDeleted();
+                northwindContext.Database.EnsureCreated();
+            }
+        }
         //https://docs.microsoft.com/zh-cn/previous-versions/sql/sql-server-2005/ms186973(v%3dsql.90)
         //https://docs.microsoft.com/zh-cn/ef/core/saving/cascade-delete
         //https://github.com/aspnet/EntityFramework.Docs/blob/live/entity-framework/core/saving/cascade-delete.md
@@ -26,27 +52,31 @@ namespace EFCore.Test
          * 如果关联实体已经被跟踪，将主实体的状态标记为删除时，关联将实体的状态也会标记为删除，执行SaveChange时，先删除关联实体，然后删除主实体；
          * 外键也可以设置不可能空。
          */
-        [InlineData(DeleteBehavior.Cascade)]
+        [InlineData(DeleteBehavior.Cascade, true)]
+        [InlineData(DeleteBehavior.Cascade, false)]
         /*
          * 如果关联实体未被跟踪，主实体的状态标记为删除，执行SaveChage时，通过数据库的行为关联数据行的外键更新为Null；
          * 如果关联实体已经被跟踪，将主实体的状态标记为删除时，关联将实体的外键被设置为Null ,同时将关联实体的状态标记为修改，执行SaveChange时，先更新关联实体的数据 ，然后删除主实体；
          * 外键不能设置不可能空字段。
          */
-        [InlineData(DeleteBehavior.SetNull)]//
+        [InlineData(DeleteBehavior.SetNull, true)]
+        [InlineData(DeleteBehavior.SetNull, false)]
         /*
          * 数据库不会执行任何行为。
          * 关联实体必须被跟踪，将主实体的状态标记为删除时，关联将实体的外键被设置为Null ,同时将关联实体的状态标记为修改，执行SaveChange时，先更新关联实体的数据 ，然后删除主实体；
          * 外键不能设置不可能空字段。
          */
-        [InlineData(DeleteBehavior.ClientSetNull)]
-     /*
-      * EF不执行任何操作，由开发人员决定关联实体的行为，可以将关联实体的状态设置为删除，也可以将关联实体的外键设置为null。
-      * 因为要修改关联实体的状态或外键的值，所以关联实体必须被跟踪。
-      */
-        [InlineData(DeleteBehavior.Restrict)]
+        [InlineData(DeleteBehavior.ClientSetNull, true)]
+        [InlineData(DeleteBehavior.ClientSetNull, false)]
+        /*
+         * EF不执行任何操作，由开发人员决定关联实体的行为，可以将关联实体的状态设置为删除，也可以将关联实体的外键设置为null。
+         * 因为要修改关联实体的状态或外键的值，所以关联实体必须被跟踪。
+         */
+        [InlineData(DeleteBehavior.Restrict, true)]
+        [InlineData(DeleteBehavior.Restrict, false)]
 
         [Theory]
-        public void Execute(DeleteBehavior behavior)
+        public void Execute(DeleteBehavior behavior, bool includeDetail)
         {
             using (var northwindContext = new NorthwindContext(behavior))
             {
@@ -75,7 +105,9 @@ namespace EFCore.Test
 
                 northwindContext.Set<Order>().Add(order);
 
+
                 northwindContext.SaveChanges();
+
 
                 orderId = order.OrderID;
                 detailId = orderDetail.DetailId;
@@ -83,21 +115,35 @@ namespace EFCore.Test
 
             using (var northwindContext = new NorthwindContext(behavior))
             {
-               var queryable = northwindContext.Set<Order>().Where(e=>e.OrderID==orderId);
+                var queryable = northwindContext.Set<Order>().Where(e => e.OrderID == orderId);
                 //if (behavior == DeleteBehavior.ClientSetNull|| behavior==DeleteBehavior.Restrict)
+                if (includeDetail)
                 {
                     queryable = queryable.Include(e => e.OrderDetails);
                 }
-                    var order = queryable.Single();//.Include(e=>e.OrderDetails).Single();
+
+                var order = queryable.Single(); //.Include(e=>e.OrderDetails).Single();
                 northwindContext.Set<Order>().Remove(order);
-                if (behavior == DeleteBehavior.Restrict)
+                if (behavior == DeleteBehavior.Restrict
+                    && order.OrderDetails != null)
                 {
                     foreach (var orderDetail in order.OrderDetails)
                     {
                         orderDetail.OrderID = null;
                     }
                 }
-                northwindContext.SaveChanges();
+
+                try
+                {
+                    northwindContext.SaveChanges();
+                    DumpSql();
+                }
+                catch (Exception)
+                {
+                    DumpSql();
+                    throw;
+                }
+
             }
 
             using (var northwindContext = new NorthwindContext(behavior))
@@ -112,6 +158,68 @@ namespace EFCore.Test
                     Assert.NotNull(orderDetail);
                 }
             }
+        }
+
+        private void DumpSql()
+        {
+            foreach (var logMessage in MyLoggerProvider.LogMessages)
+            {
+                _testOutputHelper.WriteLine("    " + logMessage);
+            }
+        }
+    }
+
+    internal class MyLoggerProvider :   ILoggerFactory
+    {
+
+        public static IList<string> LogMessages = new List<string>();
+        public ILogger CreateLogger(string categoryName) => new SampleLogger();
+        public void AddProvider(ILoggerProvider provider)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private class SampleLogger : ILogger
+        {
+            public bool IsEnabled(LogLevel logLevel) => true;
+
+            public void Log<TState>(
+                LogLevel logLevel, EventId eventId, TState state, Exception exception,
+                Func<TState, Exception, string> formatter)
+            {
+                if (eventId.Id == RelationalEventId.CommandExecuting.Id)
+                {
+                    var message = formatter(state, exception);
+                    var commandIndex = Math.Max(message.IndexOf("UPDATE"), message.IndexOf("DELETE"));
+                    if (commandIndex >= 0)
+                    {
+                        var truncatedMessage = message.Substring(commandIndex, message.IndexOf(";", commandIndex) - commandIndex).Replace(Environment.NewLine, " ");
+
+                        for (var i = 0; i < 4; i++)
+                        {
+                            var paramIndex = message.IndexOf($"@p{i}='");
+                            if (paramIndex >= 0)
+                            {
+                                var paramValue = message.Substring(paramIndex + 5, 1);
+                                if (paramValue == "'")
+                                {
+                                    paramValue = "NULL";
+                                }
+
+                                truncatedMessage = truncatedMessage.Replace($"@p{i}", paramValue);
+                            }
+                        }
+
+                        LogMessages.Add(truncatedMessage);
+                    }
+                }
+            }
+
+            public IDisposable BeginScope<TState>(TState state) => null;
         }
     }
 }
--- mytest/EFCore.Test/NorthwindContext.cs
+++ mytest/EFCore.Test/NorthwindContext.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.SqlClient;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
 
 namespace EFCore.Test
 {
@@ -13,56 +16,64 @@ namespace EFCore.Test
         {
             DeleteBehavior = deleteBehavior;
         }
+
         public NorthwindContext()
         {
         }
+
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
 
+        public override int SaveChanges()
+        {
+            MyLoggerProvider.LogMessages.Clear();
+            return base.SaveChanges();
+        }
 
-
-        public DeleteBehavior DeleteBehavior { get;}
+        public DeleteBehavior DeleteBehavior { get; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
             {
-                DataSource = ".",
+                DataSource = "192.168.10.226",
                 InitialCatalog = "Northwind_Test",
                 UserID = "sa",
                 Password = "w1!"
             };
             optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString).ReplaceService<IModelCacheKeyFactory, DynamicModelCacheKeyFactory>();
-
-          //  optionsBuilder.UseLazyLoadingProxies();
-
+            optionsBuilder.EnableSensitiveDataLogging();
+            //  optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseLoggerFactory(new MyLoggerProvider());
             base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Order>(b =>
-            {
-                b.Property(e => e.OrderID);
- 
-                b.Property(e => e.OrderDate);
-                b.HasQueryFilter(e => e.OrderID > 0);
-                b.HasIndex(e => e.OrderID).IsUnique();
-
-               b.HasMany<OrderDetail>(e=>e.OrderDetails).WithOne(e=>e.Order).HasForeignKey(od=>od.OrderID).OnDelete(this.DeleteBehavior);
-            });
-       
-
-
-            modelBuilder.Entity<OrderDetail>(e =>
-            {
-                e.HasKey(od => od.DetailId);
-                e.ToTable("Order Details");
-                if(DeleteBehavior==DeleteBehavior.Cascade)
+            modelBuilder.Entity<Order>(
+                builder =>
                 {
-                    e.Property(od => od.OrderID).IsRequired();
-                }
-            });
+                    builder.Property(e => e.OrderID);
+
+                    builder.Property(e => e.OrderDate);
+                    builder.HasQueryFilter(e => e.OrderID > 0);
+                    builder.HasIndex(e => e.OrderID).IsUnique();
+
+                    builder.HasMany<OrderDetail>(e => e.OrderDetails).WithOne(e => e.Order).HasForeignKey(e => e.OrderID).OnDelete(this.DeleteBehavior);
+                });
+
+
+
+            modelBuilder.Entity<OrderDetail>(
+                e =>
+                {
+                    e.HasKey(od => od.DetailId);
+                    e.ToTable("Order Details");
+                    if (DeleteBehavior == DeleteBehavior.Cascade)
+                    {
+                        e.Property(od => od.OrderID).IsRequired();
+                    }
+                });
         }
     }
 
@@ -71,8 +82,8 @@ namespace EFCore.Test
         public object Create(DbContext context)
         {
             var northwindContext = (NorthwindContext)context;
-            return   (
-                northwindContext.GetType(),northwindContext.DeleteBehavior
+            return (
+                northwindContext.GetType(), northwindContext.DeleteBehavior
             );
         }
     }
@@ -81,54 +92,61 @@ namespace EFCore.Test
 
     public class Order
     {
-        private readonly ILazyLoader _lazyLoader;
-        private ICollection<OrderDetail> _orderDetails;
+        //private readonly ILazyLoader _lazyLoader;
+        //private ICollection<OrderDetail> _orderDetails;
 
-        public Order(ILazyLoader lazyLoader)
-        {
-            _lazyLoader = lazyLoader;
-        }
-        public Order()
-        {
-        }
+        //public Order(ILazyLoader lazyLoader)
+        //{
+        //    _lazyLoader = lazyLoader;
+        //}
+
+        //public Order()
+        //{
+        //}
+
         public int OrderID { get; set; }
 
         public string Name { get; set; }
 
         public DateTime? OrderDate { get; set; }
 
-        public  ICollection<OrderDetail> OrderDetails
-        {
-            get => _lazyLoader?.Load(this,ref _orderDetails)?? _orderDetails;
-            set => _orderDetails = value;
-        }
+        //[ForeignKey(nameof(OrderDetail.OrderID))]
+
+        //public  ICollection<OrderDetail> OrderDetails
+        //{
+        //    get => _lazyLoader?.Load(this,ref _orderDetails)?? _orderDetails;
+        //    set => _orderDetails = value;
+        //}
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
     }
 
     public class OrderDetail
     {
-        private readonly ILazyLoader _lazyLoader;
-        private Order _order;
+        //private readonly ILazyLoader _lazyLoader;
+        //private Order _order;
 
-        public OrderDetail(ILazyLoader lazyLoader)
-        {
-            _lazyLoader = lazyLoader;
-        }
-        public OrderDetail( )
-        {
-           ;
-        }
+        //public OrderDetail(ILazyLoader lazyLoader)
+        //{
+        //    _lazyLoader = lazyLoader;
+        //}
+
+        //public OrderDetail()
+        //{
+        //    ;
+        //}
 
         public int DetailId { get; set; }
 
-        public int? OrderID { get;
-            set; }
+        public int? OrderID { get; set; }
         public int ProductID { get; set; }
- 
 
-        public  Order Order
-        {
-            get => _lazyLoader?.Load(this, ref _order)?? _order;
-            set => _order = value;
-        }
+
+        //public  Order Order
+        //{
+        //    get => _lazyLoader?.Load(this, ref _order)?? _order;
+        //    set => _order = value;
+        //}
+        public Order Order { get; set; }
     }
 }
